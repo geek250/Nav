220185150041a07e8b64d6e790347e3a7eb3f9e8
geek250 <honogeek@gmail.com>
2021-08-10 18:58:57 +0200

    first commit

---
+++ asset/main.js
@@ -0,0 +1,6 @@
+const togglebtn= document.getElementsByClassName('toggle-btn')[0]
+const navbarlink= document.getElementsByClassName('nav-link')[0]
+togglebtn.addEventListener('click', () =>{
+    navbarlink.classList.toggle-btn('active')
+
+})
